c51983c028065e810e5cf20a1074a084c457afb1
Suton <sutonommp@gmail.com>
2026-03-24 10:44:55 +0800

    Add seeder for 20 dummy pegawai

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Custom Seeder SIPEGA untuk menghasilkan 20 data Pegawai random
+        $colors = ['Biru', 'Hijau', 'Kuning', 'Merah'];
+        
+        for ($i = 1; $i <= 20; $i++) {
+            $score = rand(40, 100);
+            
+            if ($score >= 91) $color = 'Biru';
+            elseif ($score >= 76) $color = 'Hijau';
+            elseif ($score >= 61) $color = 'Kuning';
+            else $color = 'Merah';
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            User::create([
+                'name' => 'Pegawai Dummy ' . $i,
+                'email' => 'pegawai' . $i . '@sipega.com',
+                'password' => bcrypt('password'), // password standar "password"
+                'role' => 'Pegawai',
+                'nip' => '1980' . rand(100000, 999999) . rand(1000, 9999) . '100' . rand(1, 9),
+                'performance_score' => $score,
+                'performance_color' => $color,
+            ]);
+        }
     }
 }
